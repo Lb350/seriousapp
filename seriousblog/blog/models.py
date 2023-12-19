9a191c17d2befcb264f25e0413dc211dc263784b
lb350 <lb3509@gmail.com>
2023-12-19 22:49:26 +0400

    in admin.py create @admin.register (display, filter,  search, ...)

--- seriousblog/blog/models.py
+++ seriousblog/blog/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
-
-    tittle = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now())
     created = models.DateTimeField(auto_now_add=True)
@@ -24,5 +28,4 @@ class Post(models.Model):
         ]
 
     def __str__(self):
-        return self.tittle
-
+        return self.title
